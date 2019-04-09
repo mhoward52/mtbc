b60bb7696f3d8ca4baa8dd336bd4cbcab533d119
Michael Howard <michael.howard.pha@icloud.com>
2019-04-09 09:47:59 -0500

    PHP/various

---
+++ php/hello.php
@@ -0,0 +1,19 @@
+<?php
+
+echo "Hey Young World";
+$msg = 'Hi Mr. World'
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <title>Contact</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+       <h1> <?php echo 'Hello World'; ?> </h1>
+       <h2> <?php echo $msg; ?> </h2>
+    </body>
+</html>
